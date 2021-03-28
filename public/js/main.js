10b68efd9f362949fcadb7fb85760aa9cf241cb5
jaysuthar67 <jaysuthar67@gmail.com>
2021-03-28 17:33:44 +0530

    ..

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,23 @@
 function validate() {
-    alert(1);
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("passwd").value;
+
+    console.log(email);
+    console.log(password);
 }
+
+
+
+// firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+//     window.location.href = "dashboard/";
+// }).catch(function (error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     if (errorCode === 'auth/wrong-password') {
+//         document.getElementById("err").classList.remove("hide");
+//     } else if (errorCode === 'auth/invalid-email') {
+//         document.getElementById("err").classList.remove("hide");
+//     } else {
+//         console.log(errorCode);
+//     }
+// });
