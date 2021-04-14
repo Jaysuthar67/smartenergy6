ad31581a33a66ec674ad2a5c51c96d1f8dd5de78
jaysuthar67 <jaysuthar67@gmail.com>
2021-04-14 20:27:14 +0530

    -JS

--- public/dashboard/js/class1.js
+++ public/dashboard/js/class1.js
@@ -1,2 +1,13 @@
+// const startStopCam = document.querySelector('.startStopCam');
+var cam = false;
 
+function startStopCam() {
 
+    if (cam == false) {
+        cam = true;
+        startDetection();
+    } else {
+        cam = false;
+        stopDetection();
+    }
+}
--- public/dashboard/js/personDetection.js
+++ public/dashboard/js/personDetection.js
@@ -1,6 +1,8 @@
 let camera;
 var H = 240;
 var W = 320;
+var stop;
+// var loop1;
 let sketch = function (p) {
     p.setup = function () {
         p.createCanvas(W, H);
@@ -13,11 +15,29 @@ let sketch = function (p) {
     p.draw = function () {
         p.image(camera, 0, 0, W, H);
     }
+    stop = function () {
+        p.noLoop();
+        p.clear();
+    }
 
 };
 
 function startDetection(w, h) {
-     H = h;
-     W = w;
-    new p5(sketch, window.document.getElementById('camCanvasId'));
+    if (w || h) {
+        H = h;
+        W = w;
+    }
+    var p5Js = null
+    if (p5Js != null) {
+        p5Js.Loop();
+    } else {
+        p5Js = new p5(sketch, window.document.getElementById('camCanvasId'));
+    }
+
+    // new p5(sketch, window.document.getElementById('camCanvasId'));
+
+}
+
+function stopDetection() {
+    stop();
 }
