205a73304abd65b89863c5079d972696f8d31511
jaysuthar67 <jaysuthar67@gmail.com>
2021-04-14 19:43:52 +0530

    -JS

--- public/dashboard/js/personDetection.js
+++ public/dashboard/js/personDetection.js
@@ -1,15 +1,23 @@
-let capture;
-
+let camera;
+var H = 240;
+var W = 320;
 let sketch = function (p) {
     p.setup = function () {
-        p.createCanvas(320, 240);
+        p.createCanvas(W, H);
         p.background(0);
-
+        camera = p.createCapture(p.VIDEO);
+        camera.hide();
+        camera.size(W, H)
     }
 
     p.draw = function () {
-        
+        p.image(camera, 0, 0, W, H);
     }
 
 };
-new p5(sketch, window.document.getElementById('camCanvasId'));
+
+function startDetection() {
+    var H = 240;
+    var W = 320;
+    new p5(sketch, window.document.getElementById('camCanvasId'));
+}
