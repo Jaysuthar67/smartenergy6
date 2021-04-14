b6f838a0b1c3a64c654a814c75c3c8253151f0ee
jaysuthar67 <jaysuthar67@gmail.com>
2021-04-14 18:08:58 +0530

    -JS

---
+++ public/dashboard/js/personDetection.js
@@ -0,0 +1,8 @@
+let sketch = function (p) {
+    p.setup = function () {
+        p.createCanvas(100, 100);
+        p.background(0);
+
+    }
+};
+new p5(sketch, window.document.getElementById('camCanvasId'));
